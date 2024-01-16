9aa232b47315d5245778c2cd46f656de14bde93c
sajid2123 <sajidmalhi9@gmail.com>
2024-01-16 21:22:05 +0100

    Sajid 21:21 16-01-2024

---
+++ src/app/radiologo/tabla-citas/tabla-citas.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+interface Cita {
+  sip: string;
+  nombre: string;
+  apellido: string;
+  hora: string;
+}
+
+@Component({
+  selector: 'app-tabla-citas',
+  templateUrl: './tabla-citas.component.html',
+  styleUrls: ['./tabla-citas.component.css']
+})
+export class TablaCitasComponent {
+  citas: Cita[] = [
+    { sip: '1234', nombre: 'Ana', apellido: 'García', hora: '08:00' },
+    { sip: '5678', nombre: 'Luis', apellido: 'Martínez', hora: '09:00' },
+    { sip: '1235', nombre: 'Carlos', apellido: 'López', hora: '08:30' },
+    { sip: '5679', nombre: 'María', apellido: 'Gómez', hora: '09:30' },
+    { sip: '1253', nombre: 'Juan', apellido: 'Pérez', hora: '10:00' },
+    { sip: '5687', nombre: 'Sofía', apellido: 'Ruiz', hora: '10:30' },
+    { sip: '1234', nombre: 'Ana', apellido: 'García', hora: '08:00' },
+    { sip: '5678', nombre: 'Luis', apellido: 'Martínez', hora: '09:00' },
+    { sip: '1235', nombre: 'Carlos', apellido: 'López', hora: '08:30' },
+    { sip: '5679', nombre: 'María', apellido: 'Gómez', hora: '09:30' },
+    { sip: '1253', nombre: 'Juan', apellido: 'Pérez', hora: '10:00' },
+    { sip: '5687', nombre: 'Sofía', apellido: 'Ruiz', hora: '10:30' }
+  ];
+}
