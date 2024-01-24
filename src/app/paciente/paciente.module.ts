bc88508c9dd721d15a27cc228df7ef52da58efb1
seokkjj83 <seokkjj83@gmail.com>
2024-01-24 16:59:49 +0100

    Resolver conflictos después de merge con esteban2

--- src/app/paciente/paciente.module.ts
+++ src/app/paciente/paciente.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
-import { TablaPacienteComponent } from './tabla-paciente/tabla-paciente.component';
-=======
 import { TablaCitasPacientesComponent } from './tabla-citas-pacientes/tabla-citas-pacientes.component';
->>>>>>> esteban
 import { GeneralModule } from '../general/general.module';
 import { AppRoutingModule } from '../app-routing.module';
 import { DataTablesModule } from 'angular-datatables';
@@ -13,12 +9,8 @@ import { DataTablesModule } from 'angular-datatables';
 
 @NgModule({
   declarations: [
-<<<<<<< HEAD
-    TablaPacienteComponent
-=======
     TablaCitasPacientesComponent,
     TablaCitasPacientesComponent
->>>>>>> esteban
   ],
   imports: [
     CommonModule,
