c554a38718906159842cbdf90f8d5534e0164702
GMP22 <gemepe28@gmail.com>
2024-02-06 10:11:50 +0100

    Diseño de Pasos de Formulario Actualizado

--- src/app/administrativo/formulario-alta-paciente/formulario-alta-paciente.component.ts
+++ src/app/administrativo/formulario-alta-paciente/formulario-alta-paciente.component.ts
@@ -12,22 +12,27 @@ export class FormularioAltaPacienteComponent {
                                   
     cambiarSeccion(accion:string){
       let confirmar:boolean = false;
-      let columnas = $("#seccionesFormulario").find("div.col-4");
-      $(columnas[this.seccionFormulario]).removeClass("bg-success");
-
+      let columnas = $("#seccionesFormulario").children();
+      $(columnas[this.seccionFormulario]).removeClass("border-warning");
+      $(columnas[this.seccionFormulario]).removeClass("border-3");
       switch(accion){
         case 'cancelar':
             this.seccionFormulario = 0;
         break;
         case 'retroceder':
-            this.seccionFormulario--;
+          this.quitarBarra(this.seccionFormulario, columnas);
+            this.seccionFormulario-=2;
+            
             if (this.seccionFormulario != 2) {
               $(".col").find("input").prop("readonly", false);
             } 
-            $(columnas[this.seccionFormulario]).addClass("bg-success");
+            $(columnas[this.seccionFormulario]).addClass("border-warning");
+            $(columnas[this.seccionFormulario]).addClass("border-3");
         break;
         case 'avanzar':
-            this.seccionFormulario++;
+          
+            this.seccionFormulario+=2;
+            this.meterBarra(this.seccionFormulario, columnas);
             if (this.seccionFormulario == 2) {
               $(".col").find("input").prop("readonly", true);
             } 
@@ -39,6 +44,24 @@ export class FormularioAltaPacienteComponent {
             */
         break;
       }
-      $(columnas[this.seccionFormulario]).addClass("bg-success");
+      $(columnas[this.seccionFormulario]).addClass("border-warning");
+      $(columnas[this.seccionFormulario]).addClass("border-3");
     }
+
+    meterBarra(posicion:number, entrada:JQuery<HTMLElement>){
+        for (let index = 0; index < posicion; index++) {
+
+          $('#'+entrada[index].id).addClass("border-warning");
+          $('#'+entrada[index].id).addClass("border-3");
+          console.log(entrada[index].id);
+        }
+    }
+
+    quitarBarra(posicion:number, entrada:JQuery<HTMLElement>){
+      for (let index = entrada.length-1; index > posicion; index--) {
+        $('#'+entrada[index].id).removeClass("border-warning");
+        $('#'+entrada[index].id).removeClass("border-3");
+        console.log(entrada[index].id);
+      }
+  }
 }
